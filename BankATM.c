26fb4423b29e981e3ebb573eb75afc303288360e
srinivasrao99 <srinivasaraogude7@gmail.com>
2024-02-16 13:07:28 +0530

    Create Function Modified

--- BankATM.c
+++ BankATM.c
@@ -7,7 +7,9 @@ struct ATM{
     int acno;
     char name[50];
     int amount;
-}; struct ATM A1;
+};
+
+struct ATM A1;
 
 void design();
 void createacc();
@@ -19,41 +21,39 @@ int main(){
     clrscr();
     int count = 0;
     char ch1, ch2, ch3, ch4;
-    pin:    printf("Enter your pin: ");
+
+    pin: printf("Enter your pin: ");
     
-    ch1 = getch();
-    printf("*");
-    ch2 = getch();
-    printf("*");
-    ch3 = getch();
-    printf("*");
-    ch4 = getch();
-    printf("*");
+    ch1 = getch();  printf("*");
+    ch2 = getch();  printf("*");
+    ch3 = getch();  printf("*");
+    ch4 = getch();  printf("*");
+
     if (ch1 = 'd' && ch2 == 'a' && ch3 == 't' && ch4 == 'a'){ //dummy pin for development
+        
         int choice;
+
         do{
             design();
             printf("\n Enter your choice: ");
             scanf("%d", &choice);
 
             switch(choice){
-                case 1: createacc();break;
-                case 2: depoist();break;
-                case 3: withd();break;
-                case 4: balance();break;
-                case 5: break;//exit(0);
+                case 1  : createacc();  break;
+                case 2  : depoist();    break;
+                case 3  : withd();      break;
+                case 4  : balance();    break;
+                case 5  : break;        //exit(0);
                 default : printf("Invalid Choice \n");
             }
         }while(choice != 5);
+
     }else{
-        printf("\nInvaild Pin\n");
+        printf("\nInvaild Pin\n"); 
         count++;
-        if(count >= 3){
-            exit(0);
-        }else{
-            goto pin;
-            }
-        }
+        if(count >= 3){exit(0);}else{goto pin;}
+    }
+
     return 0;
 }
 
@@ -68,10 +68,9 @@ void design (){
 }
 
 void createacc(){
-    //printf("Enter Account Number");
-    //scanf("%d", &A1.acno);
-    //printf("Enter Your Name");
-    //scanf("%d",&A1.name);
+    printf("Enter Account Number: ");   scanf("%d", &A1.acno);
+    printf("Enter Your Name: ");        scanf("%s", &A1.name);
+    printf("Enter Amount: ");           scanf("%d", &A1.amount);
 }
 
 void depoist(){
